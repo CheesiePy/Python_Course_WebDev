7a26599f1bbfcf8d5990fae1777ba599e854f50e
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-29 20:02:26 +0200

    update 29/01/23

--- input.py
+++ input.py
@@ -1,5 +1,23 @@
 
-first_name = input("please enter first name: ")
+# first_name = input("please enter first name: ")
+
+# last_name = input()
+
+# age = input("please enter age:")
+
+# if age.isdigit():
+#     print(int(age) + 10)
+
+n =  input("enter a number: ")
+print("your number: ", n)
+n = int(n) # convert str to int
+n = n**2
+print("your number to the power of 2:" , n)
+
+
+
+
+
 
 
 # [Q1] set first_name, last_name, age, height from user input
