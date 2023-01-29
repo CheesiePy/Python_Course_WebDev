3374fdbf193a20b4442d9e9242f401f7e0721fcb
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-29 20:26:14 +0200

    update 29/01/23

--- input.py
+++ input.py
@@ -8,12 +8,26 @@
 # if age.isdigit():
 #     print(int(age) + 10)
 
-n =  input("enter a number: ")
-print("your number: ", n)
-n = int(n) # convert str to int
-n = n**2
-print("your number to the power of 2:" , n)
+# n =  input("enter a number: ")
+# print("your number: ", n)
+# n = int(n) # convert str to int
+# n **= 2
+# print("your number to the power of 2:" , n)
 
+# n = input("please enter 3-digit number: ")
+# n = int(n)
+# sum = n%10
+# n //= 10
+# sum += n%10
+# n //= 10
+# sum += n%10
+# print(sum)
+
+n = input("please enter 3-digit number: ")
+x = n[0]
+y = n[1]
+z = n[2]
+print(int(x) + int(y) + int(z))
 
 
 
