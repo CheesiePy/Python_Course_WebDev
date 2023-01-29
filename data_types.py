dcec73bc5dffce006b2ac21c0060af1b17d1d69b
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-29 18:59:02 +0200

    update data_types.py

---
+++ data_types.py
@@ -0,0 +1,43 @@
+## varibles in python are lower case and we are using snake_case to declare multiple words varibles 
+
+Name = "may" # we Use UpperCase when we want to declair a global concidered a bad practice
+
+first_name = ""
+
+last_name = ""
+
+age = 29
+
+height = 1.80
+
+# to find out the data type use the function type()
+
+# to get output out off our sript we will use the print() function 
+
+# [example] make python print your full name:
+
+print(first_name, last_name)
+
+# [example] make python print the type of a varible
+
+print(type(height), type(age)) # run to see results
+
+
+# [Q1] :
+    # create a var that represent the number of siblings you have
+
+# [Q2] :
+    # print the type of the var you created in Q1
+
+# [Q3] :
+    # print your age in 10 years from now by using the age var
+
+# [Q4] :
+    # print your age 10 years ago by using the age var
+
+
+
+
+
+
+
