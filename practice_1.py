6e732b3cdddd9823beee125a922b8b8c5907b6ef
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-01-31 19:03:14 +0200

    update 31/01/23

---
+++ practice_1.py
@@ -0,0 +1,45 @@
+#    Printing numbers 1 to 10 using a for loop:
+
+#    Calculating the sum of numbers 1 to 100 using a for loop:
+
+#    Printing all even numbers from 1 to 20 using a for loop:
+
+#    Printing the multiplication table of a given number using a for loop:
+
+#    Printing the elements of a list using a for loop:
+
+
+
+
+
+# Harder Q'z
+
+# [Q1] Printing the first n fibonacci numbers using a for loop:
+
+# [Q2] Printing the prime numbers between a given range using a for loop:
+
+# [Q3] {fix the bugs!} Printing the elements of a nested list using a for loop:
+
+numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for lst in numbers:
+    for num in lst:
+    print(lst)
+
+#[Q4] { fix the bugs! } Flattening a nested list using a for loop:
+
+
+numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flat_list = []:
+    for lst in numbers:
+        for num in lst{
+list.append(num)
+        }    
+print(FlatList);
+
+
+#[Q5] Printing the frequency of each element in a list using a for loop:
+
+
+numbers = [1, 2, 3, 4, 2, 1, 4, 4, 5, 1]
+freq = {}
+
