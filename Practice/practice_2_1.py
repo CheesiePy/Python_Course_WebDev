616a25d4779f08aa5385646c565c86060b46d047
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-02-02 20:04:22 +0200

    update practice_2_1.py

---
+++ Practice/practice_2_1.py
@@ -0,0 +1,25 @@
+#     Write a program to convert the first letter of a string to uppercase.
+
+#     Write a program to count the number of occurrences of a substring in a string.
+
+#     Write a program to check if a string starts with a specific substring.
+
+#     Write a program to check if a string ends with a specific substring.
+
+#     Write a program to find the index of a substring in a string.
+
+#     Write a program to split a string into a list based on a specific character.
+
+#     Write a program to replace a specific substring in a string.
+
+#     Write a program to check if a string is alphanumeric.
+
+#     Write a program to check if a string is alphabetic.
+
+#     Write a program to check if a string is numeric.
+
+#     Write a program to check if a string is lowercase.
+
+#     Write a program to check if a string is uppercase.
+
+#     Write a program to strip whitespace from the beginning and end of a string.
