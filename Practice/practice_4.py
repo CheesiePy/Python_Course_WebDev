eb2e146485b74f122d4d939774e4a219650bd9a4
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-02-07 19:11:46 +0200

    update 07/02/23

---
+++ Practice/practice_4.py
@@ -0,0 +1,11 @@
+# Create a list of the squares of numbers from 1 to 10 using a list comprehension.
+
+# Create a list of numbers from 1 to 10 that are divisible by 3 using a list comprehension.
+
+d3_list = [x for x in range(1,11) if x % 3 == 0]
+
+# Create a list of the first 10 even numbers using a list comprehension.
+
+# Create a list of all the uppercase letters in the string "Hello, World!" using a list comprehension.
+
+# Create a list of the lengths of the words in the string "The quick brown fox jumps over the lazy dog" using a list comprehension
