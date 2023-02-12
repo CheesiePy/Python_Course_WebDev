dc1c42b7e93a0eee15a625ea611a3fa90a9878de
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-02-12 18:06:36 +0200

    update functions.py

---
+++ Subjects/functions.py
@@ -0,0 +1,17 @@
+# functions in python 
+
+def f():
+    print("f")
+
+
+def g(a):
+    print(f'g({a})')
+
+
+def h(a, b):
+    print(f'h({a}, {b})')
+
+def c(a=1, b=2):
+    print(f'c({a}, {b})')
+
+
