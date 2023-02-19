ce36a8ad3a4c85466a533c7f8014021ff15d388a
May Lindenberg <65390530+CheesiePy@users.noreply.github.com>
2023-02-19 19:27:35 +0200

    update 19/02/23

---
+++ Modules/flask_app.py
@@ -0,0 +1,20 @@
+# flask basic app
+# pip install flask
+from flask import Flask
+
+app = Flask(__name__) # create flask app
+
+
+@app.route('/home') # route to home page
+def home():
+    return "Hello World"
+
+
+@app.route('/login') # route to home page
+def login():
+    return 'please log in'
+
+def main():
+    app.run(debug=True) # run flask app
+
+main()
